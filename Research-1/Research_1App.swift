56ab33e5103e3949f69596aed90388cdad689c77
CWASH2002 <csw11@email.sc.edu>
2023-10-15 17:06:48 -0400

    Initial Commit

---
+++ Research-1/Research_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Research_1App.swift
+//  Research-1
+//
+//  Created by Cameron Washington on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Research_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
